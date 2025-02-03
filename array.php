d38720bddae86417f59f1b23f322c67c430e6c62
techbire <anshtechnical@gmail.com>
2025-02-03 11:39:25 +0530

    added

---
+++ array.php
@@ -0,0 +1,15 @@
+<?php
+$second=array("hello","ansh","and","harsh");
+$first=["hey","b.tech","year"];
+print_r(array_merge($first,$second));
+
+$a = array(22, 13, 43, 5);
+
+echo "Max: " . max($a) . "\n";
+echo "Min: " . min($a) . "\n";
+
+sort($a);
+echo "Sorted Array: ";
+print_r($a);
+
+?>
